318f115f365764e426109ea5ad4d7fd60a9b31b0
bdcorps <sunnyashiin@gmail.com>
2014-05-08 17:26:56 -0400

    Update team.java

--- src/team.java
+++ src/team.java
@@ -6,13 +6,13 @@ public class Team {
 	private int seed ; //playoff seed(1-8)
 	//stats
 	private int points;//points
- +	private int assists;//assists
- +	private int offRebounds;//offensive rebounds
+ 	private int assists;//assists
+ 	private int offRebounds;//offensive rebounds
  	private int defRebounds;//defensive rebounds
- +	private int steals;//steals
- +	private int blocks;//block
- +	private int fouls;//fouls
- +	private int turnovers;//points
+ 	private int steals;//steals
+ 	private int blocks;//block
+ 	private int fouls;//fouls
+ 	private int turnovers;//points
   	private int fgMade; //field goals made
   	private int fgAttempt; //field goals attempted
   	private int ftMade; //free throws made
@@ -22,16 +22,16 @@ public class Team {
 	//efficiency
 	private double win_loss = wins / gPlay;//winning percentage
 	private double ppg = gPlay / points; //point int per game
- +	private double rpg = gPlay / offRebounds; //offensive rebounds per game
- +	private double rpg = gPlay / defRebounds; //defensiverebounds per game
- +	private double apg = gPlay / assists; //assists per game
- +	private double spg = gPlay / steals; //steals per game
- +	private double bpg = gPlay / block; //blocks per game
- +	private double fpg = gPlay / fouls; //fouls per game
- +	private double tpg = gPlay / turnovers; //turnovers per game
- +	private double fgPercent = fgMade / fgAttempt; //field goal percentage
- +	private double ftPercent = ftMade / ftAttempt; //free throw percentage
- +	private double threeptPercent = threeptMade / threeptAttempt; //3 point percentage
+ 	private double rpg = gPlay / offRebounds; //offensive rebounds per game
+ 	private double rpg = gPlay / defRebounds; //defensiverebounds per game
+	private double apg = gPlay / assists; //assists per game
+ 	private double spg = gPlay / steals; //steals per game
+ 	private double bpg = gPlay / block; //blocks per game
+ 	private double fpg = gPlay / fouls; //fouls per game
+ 	private double tpg = gPlay / turnovers; //turnovers per game
+ 	private double fgPercent = fgMade / fgAttempt; //field goal percentage
+ 	private double ftPercent = ftMade / ftAttempt; //free throw percentage
+ 	private double threeptPercent = threeptMade / threeptAttempt; //3 point percentage
 	
 	private boolean divLeader; //division leader (true =division leader)
 
